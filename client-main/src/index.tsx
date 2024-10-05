c1515dd653138cf5562e12fa4b7565aeef88d3c5
prem9900 <nagaprem180@gmail.com>
2024-10-05 17:53:50 +0530

    Update index.tsx with footer component

--- client-main/src/index.tsx
+++ client-main/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-
+  <BrowserRouter> {/* Wrap App with BrowserRouter */}
     <App />
- 
+  </BrowserRouter>
 );
-
