7aecaf910df997f9dc2bec828c146283f01b3874
prem9900 <nagaprem180@gmail.com>
2024-10-05 21:59:01 +0530

    Update index.js with latest changes

--- client-main/src/index.js
+++ client-main/src/index.js
@@ -1,5 +1,6 @@
-import { jsx as _jsx } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(_jsx(App, {}));
+root.render(_jsxs(BrowserRouter, { children: [" ", _jsx(App, {})] }));
